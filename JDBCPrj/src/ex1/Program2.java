9f8a26e2eedb42bc29602dc3d3a9dbf49c5ffda8
hwh <sorrel012@gmail.com>
2023-02-24 23:31:05 +0900

    feat: read dbms data

---
+++ JDBCPrj/src/ex1/Program2.java
@@ -0,0 +1,41 @@
+package ex1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class Program2 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		String url = "jdbc:oracle:thin:@localhost:1521/xe";
+		String sql = "SELECT * FROM NOTICE";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "SORREL", "olian8982e");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next()) {
+			int id = rs.getInt("ID");
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regDate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
+			int hit = rs.getInt("hit");
+					
+			if(hit >= 10) {
+				System.out.printf("id: %d, title: %s, writerId: %s, redDate: %s, content: %s, hit: %d\n", id, title, writerId, regDate, content, hit);
+			}
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
+		
+	} //main
+	
+}
